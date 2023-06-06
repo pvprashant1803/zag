819925ef6c05f2fa5f4dfb7964e1a34fc094a112
pvprashant1803 <pvprashant1803@gmail.com>
2023-06-06 11:20:30 +0530

    Deploying React App

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import Dashboard from './pages/Dashboard';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
           <Route index element={<Home />} />
         <Route path="/productpage" element={<ProductPage />} />
